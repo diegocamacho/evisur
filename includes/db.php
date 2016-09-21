bd00614552f842bf0b387569e4f6e376e7ed579a
Diego Camacho <diegocamacho2.0@gmail.com>
2016-09-21 15:39:28 -0500

    BD en Web

--- includes/db.php
+++ includes/db.php
@@ -1,17 +1,17 @@
 <?php
 //error_reporting(0);
-/* Conexión en entorno local */
+/* Conexión en entorno local 
 $servidor="localhost";
 $usuario="root";
 $clave="root";
-$base="evisur_app";
+$base="evisur_app";*/
 
-/* Conexión en producción 
-$servidor="localhost";
+/* Conexión en producción */
+$servidor="epicmedia.pro";
 $usuario="epicmedi_digma";
 $clave="Studio16";
 $base="epicmedi_evisur";
-*/
+
 $conexion = @mysql_connect ($servidor,$usuario,$clave) or die ("Ocurrió un error al conectarse.");
 @mysql_select_db($base) or die ("No BD ");
 ?>
